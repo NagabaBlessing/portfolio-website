63190308143b6476ee5212b3dc4d42bd8f511583
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-05-15 18:29:59 +0300

    Configured navigation and fixed bugs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Navbar from './components/navigationbar';
 import Home from './page/Home';
 import Projects from './page/projects';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
    <>
-   <Navbar/>
-    <Projects/>
+  <Router>
+  <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  </Router>
    </>
   );
 }
--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -7,12 +8,12 @@ export default function Navbar({ fixed }) {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-transparent mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
+            <Link
               className="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-black"
-              href="#pablo"
+              to="/"
             >
               Sabrina Kitaka
-            </a>
+            </Link>
             <button
               className="text-black cursor-pointer text-xl leading-none px-3 py-1 border 
               border-solid border-transparent rounded bg-transparent block lg:hidden outline-none
@@ -32,31 +33,31 @@ export default function Navbar({ fixed }) {
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
+                <Link
                   className="px-3 py-2 flex items-center text-xs uppercase 
                   font-bold leading-snug text-black hover:opacity-75"
-                  href="#pablo"
+                  to="/about"
                 >
                   <i className="fab fa-facebook-square text-lg leading-lg text-black opacity-75"></i><span className="ml-2 text-lg">About</span>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black 
                   hover:opacity-75"
-                  href="#pablo"
+                  to="/projects"
                 >
                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2 text-lg">Projects</span>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold 
                   leading-snug text-black hover:opacity-75"
-                  href="#pablo"
+                  to="/contact-me"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-black opacity-75"></i><span className="ml-2 text-lg">Contact me</span>
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
