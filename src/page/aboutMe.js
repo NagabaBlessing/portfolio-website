93401e8fc6bf54d1612541374b7150bbb6e82962
NagabaBlessing <nagabablessing1@gmail.com>
2023-05-15 18:14:46 -0700

    blessy added

---
+++ src/page/aboutMe.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+export default function AboutMe() {
+ return (
+    <div className="grid md:grid-cols-2 gap-3">
+        <div className="ms-4">
+            
+            <h1 className="text-8xl my-4">About me</h1>
+            <p className="text-gray-500 text-4xl my-4">Sabrina Kitaka is an author of The Real Persuit of Excellence. Her autobiography and also a life coach and mentor.</p>
+        <button className="bg-red-500 hover:bg-red-700 py-2 px-4 rounded ">Resume</button>&nbsp;
+        
+        </div>
+        <div>
+        </div>
+    </div>
+
+    
+ )
+}
---
+++ src/page/contactMe.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function ContactMe() {
+    return (
+    <>
+    <div className="flex justify-center">
+        <h1 className="text-3xl font-bold tracking-wide">Projects</h1>
+    </div>
+    <div className="flex justify-center">
+       <div className="bg-red-500 w-16 h-1 rounded mt-2">
+
+       </div>
+    </div>
+    
+<form>
+    <div className="grid gap-6 mb-6 md:grid-cols-2">
+        <div>
+            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required>
+        </div>
+
+       
+       
+        
+    </div>
+    <div className="mb-6">
+        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+        <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required>
+    </div> 
+    </div>
+    
+<label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message</label>
+<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+    
+    
+    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
+
+<button className="bg-red-500 hover:bg-red-700 py-2 px-4 rounded ">Send</button>&nbsp;
+
+   
+
+    </>
+    )
+}
