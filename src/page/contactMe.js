4808fd2e406195f4d20dade749f3c7dc82f2e408
NagabaBlessing <nagabablessing1@gmail.com>
2023-05-16 17:59:47 -0700

    blessy

--- src/page/contactMe.js
+++ src/page/contactMe.js
@@ -13,29 +13,29 @@ export default function ContactMe() {
     </div>
     
 <form>
-    <div className="grid gap-6 mb-6 md:grid-cols-2">
+    <div className="mb-6">
         <div>
-            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
+            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Name</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
         </div>
     </div>
     <div>
         <div className="mb-6">
-            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required/>
+            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Email</label>
+            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
         </div> 
     </div>
 
     
-<label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message</label>
-<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+<label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Message</label>
+<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-transparent-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
 
     
     
-    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    
 </form>
 
-<button className="bg-red-500 hover:bg-red-700 py-2 px-4 rounded ">Send</button>&nbsp;
+<button className="bg-red-500 hover:bg-red-700 py-2 px-4 rounded mt-4">Send</button>&nbsp;
 
    
 
