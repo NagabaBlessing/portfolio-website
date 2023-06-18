478c5b6af0fb6d003c212f177f082cc8e3df0c25
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-06-18 18:10:18 +0300

    Added fonts

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -120,5 +120,11 @@ export default function Navbar(props,{ fixed }) {
 
 
 const drawerWidth = 240;
-const navItems = [{name:"Home", link:"/"},{name:"About", link:"/about"},{name:"Projects", link:"/projects"}, {name:"Contact Me", link:"/contact-me"}];
+const navItems = [
+  {name:"Home", link:"/"},
+  {name:"About", link:"/about"},
+  {name:"Blog", link:"/blog"},
+  {name:"Projects", link:"/projects"}, 
+  {name:"Contact Me", link:"/contact-me"}
+];
 
